3964ffeb3ab4434ce5e1865bf8f5055745d27fdc
MikiKru <michal_kruczkowski@o2.pl>
2019-11-19 11:06:13 +0100

    jfx v1

--- src/main/java/app/controller/LoginController.java
+++ src/main/java/app/controller/LoginController.java
@@ -1,7 +1,23 @@
 package app.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class LoginController {
+
+    @FXML
+    private TextField tfLogin;
+
+    @FXML
+    private PasswordField pfPassword;
+
+    @FXML
+    void loginAction(ActionEvent event) {
+
+    }
+
 }
