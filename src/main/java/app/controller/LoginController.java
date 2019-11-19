aaecf3c6127dd3b9178c482cde5d2382eb497337
MikiKru <michal_kruczkowski@o2.pl>
2019-11-19 13:21:25 +0100

    jfx v4

--- src/main/java/app/controller/LoginController.java
+++ src/main/java/app/controller/LoginController.java
@@ -1,13 +1,24 @@
 package app.controller;
 
+import app.service.LoginService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import app.model.User;
+
+import java.security.NoSuchAlgorithmException;
 
 @Controller
 public class LoginController {
+
+    private LoginService loginService;
+    @Autowired
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
 
     @FXML
     private TextField tfLogin;
@@ -16,8 +27,14 @@ public class LoginController {
     private PasswordField pfPassword;
 
     @FXML
-    void loginAction(ActionEvent event) {
-
+    void loginAction(ActionEvent event) throws NoSuchAlgorithmException {
+        User user = loginService.loginUser(
+                tfLogin.getText(), pfPassword.getText());
+        if(user != null){
+            System.out.println("Zalogowano");
+        } else {
+            System.out.println("Błąd logowania!");
+        }
     }
 
 }
