ed7a9dc1e5d99f0eeca3ed156072a9a2a0fcd174
MikiKru <michal_kruczkowski@o2.pl>
2019-11-19 11:21:14 +0100

    jfx v2

---
+++ src/main/java/app/model/User.java
@@ -0,0 +1,26 @@
+package app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data // -> dodaje get/set & toString()
+@NoArgsConstructor  // -> dodaje bezparametrowy konstruktor
+@AllArgsConstructor // -> dodaje konstruktor z wszystkimi polami
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int user_id;
+    private String login;
+    private String password;
+    @Value("true")
+    private boolean status;
+    
+}
---
+++ src/main/java/app/service/LoginService.java
@@ -0,0 +1,12 @@
+package app.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+
+    // logowanie użytkownika
+
+    // czyszczenie pól TF i PF
+
+}
